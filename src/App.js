7bdaa8660fa01defe513f69efee5380e9d4a7051
Wangdi Tenzing <wangdi457@gmail.com>
2022-02-14 16:23:51 -0500

    completed about

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import About from "./components/About/About";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Navbar />
+        <About />
       </header>
     </div>
   );
